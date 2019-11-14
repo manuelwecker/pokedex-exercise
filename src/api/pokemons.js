f3a72b7b931d448a72b7447fc70cf2e69869574c
manuelwecker <manuelwecker@web.de>
2019-11-14 11:33:29 +0100

    use own json array intead of json-package

--- src/api/pokemons.js
+++ src/api/pokemons.js
@@ -6,18 +6,21 @@
  *
  * There are some helper functions which makes this task easier, but please try to implement your own filter and search logic.
  */
-import pokemons from 'json-pokemon';
+// import pokemons from 'json-pokemon';
+import pokemons from './pokemons.json';
+
+export function getAllPokemons() {
+  return pokemons;
+}
 
 export function getPokemonsByName(pokemonName) {
-  /**
-   * pokemonName is unused.
-   * You could use this property to filter the pokemons by name.
-   * Take a look: Array.prototype.filter()
-   *
-   * Try to return all pokemons which starts with the name like: `pik` -> `Pikachu`, `Pikipek`.
-   * It should be case independend.
-   */
-  return pokemons;
+  const normalizedName = pokemonName.toLowerCase();
+  const pickPokemons = pokemons.filter(pokemon => {
+    const normalizedPokemonName = pokemon.name.toLowerCase();
+    return normalizedPokemonName.startsWith(normalizedName);
+  });
+
+  return pickPokemons;
 }
 
 export function sortPokemonsByName(pokemons, sortDirection = 'ASC') {
@@ -25,6 +28,18 @@ export function sortPokemonsByName(pokemons, sortDirection = 'ASC') {
    * `sortDirection` could be `ASC` or `DESC`. Try to reverse the results if `sortDirection` is `DESC`.
    *
    * See Array.prototype.sort()
+   * And see reverse option for arrays
    */
-  return pokemons;
+  // pokemons.sort(function(pokemonA, pokemonB) {
+  //   const nameA = pokemonA.name.toUpperCase(); // ignore upper and lowercase
+  //   const nameB = pokemonB.name.toUpperCase(); // ignore upper and lowercase
+  //   if (nameA < nameB) {
+  //     return -1;
+  //   }
+  //   if (nameA > nameB) {
+  //     return 1;
+  //   }
+  //   // names must be equal
+  //   return 0;
+  // )}
 }
