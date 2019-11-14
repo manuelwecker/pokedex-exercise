4dfca5603afb989e78966ba2621543a6cd2d84b1
manuelwecker <manuelwecker@web.de>
2019-11-14 16:04:56 +0100

    add async api function

--- src/api/pokemons.js
+++ src/api/pokemons.js
@@ -7,23 +7,50 @@
  * There are some helper functions which makes this task easier, but please try to implement your own filter and search logic.
  */
 // import pokemons from 'json-pokemon';
-import pokemons from './pokemons.json';
+// import pokemons from './pokemons.json';
 
-fetch('https://pokeapi.co/api/v2/pokemon/pikachu')
-  .then(response => response.json())
-  .then(data => console.log(data));
+let pokemons = [];
 
-export function getPokemonsByName(pokemonName) {
-  /**
-   * pokemonName is unused.
-   * You could use this property to filter the pokemons by name.
-   * Take a look: Array.prototype.filter()
-   *
-   * Try to return all pokemons which starts with the name like: `pik` -> `Pikachu`, `Pikipek`.
-   * It should be case independend.
-   */
-  return pokemons;
+export async function initPokemons() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000');
+  const data = await response.json();
+  pokemons = data.results;
 }
+
+// export function getPokemonDetails(pokemonUrl) {
+//   const response = await fetch(pokemonUrl);
+//   const data = pokemonUrl;
+//   return data;
+// }
+
+// Callback as async function
+export function getAllPokemons() {
+  return pokemons;
+  //
+  // fetch('https://pokeapi.co/api/v2/pokemon/?limit=1000')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data))
+  //   .catch(error => console.error(error));
+
+  // const response = await fetch('https://pokeapi.co/api/v2/pokemon/pikachu');
+  // const data = await response.json();
+  // console.log(data);
+  // const pokemons = data;
+  // console.log('Done');
+  // return pokemons;
+}
+
+// export function getPokemonsByName(pokemonName) {
+//   /**
+//    * pokemonName is unused.
+//    * You could use this property to filter the pokemons by name.
+//    * Take a look: Array.prototype.filter()
+//    *
+//    * Try to return all pokemons which starts with the name like: `pik` -> `Pikachu`, `Pikipek`.
+//    * It should be case independend.
+//    */
+//   return pokemons;
+// }
 
 export function getPokemonsByName(pokemonName) {
   const normalizedName = pokemonName.toLowerCase();
@@ -36,22 +63,58 @@ export function getPokemonsByName(pokemonName) {
 }
 
 export function sortPokemonsByName(pokemons, sortDirection = 'ASC') {
-  /**
-   * `sortDirection` could be `ASC` or `DESC`. Try to reverse the results if `sortDirection` is `DESC`.
-   *
-   * See Array.prototype.sort()
-   * And see reverse option for arrays
-   */
-  // pokemons.sort(function(pokemonA, pokemonB) {
-  //   const nameA = pokemonA.name.toUpperCase(); // ignore upper and lowercase
-  //   const nameB = pokemonB.name.toUpperCase(); // ignore upper and lowercase
-  //   if (nameA < nameB) {
-  //     return -1;
-  //   }
-  //   if (nameA > nameB) {
-  //     return 1;
-  //   }
-  //   // names must be equal
-  //   return 0;
-  // )}
+  if (sortDirection === 'ASC') {
+    const sortedPokemons = pokemons.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    return sortedPokemons;
+  } else if (sortDirection === 'DESC') {
+    const sortedPokemons = pokemons.sort((a, b) =>
+      a.name.localeCompare(b.name)
+    );
+    return sortedPokemons.reverse();
+  }
 }
+
+// export function sortPokemonsByName(pokemons, sortDirection = 'DESC') {
+//   const sortedPokemons = pokemons.sort((pokemonA, pokemonB) => {
+//     const nameA = pokemonA.name.toLowerCase();
+//     const nameB = pokemonB.name.toLowerCase();
+
+//     if (nameA < nameB) {
+//       return -1;
+//     }
+//     if (nameA > nameB) {
+//       return 1;
+//     }
+//     return 0;
+//   });
+//   if (sortDirection === 'DESC') {
+//     sortedPokemons.reverse();
+//   }
+//   return pokemons;
+// }
+
+// export function sortPokemonsByName(pokemons, sortDirection = 'ASC') {
+// /**
+//  * `sortDirection` could be `ASC` or `DESC`. Try to reverse the results if `sortDirection` is `DESC`.
+//  *
+//  * See Array.prototype.sort()
+//  * And see reverse option for arrays
+//  */
+//   if (sortDirection === 'ASC') {
+//     const sortedPokemons = pokemons.sort(a, b) => a.name.localeCompare(b.name);
+//   }
+//    pokemons.sort(function(pokemonA, pokemonB) {
+//     const nameA = pokemonA.name.toUpperCase(); // ignore upper and lowercase
+//     const nameB = pokemonB.name.toUpperCase(); // ignore upper and lowercase
+//     if (nameA < nameB) {
+//       return -1;
+//     }
+//     if (nameA > nameB) {
+//       return 1;
+//     }
+//     // names must be equal
+//     return 0;
+//   )}
+// )}
--- src/index.js
+++ src/index.js
@@ -1,52 +1,72 @@
+// require to use async and await
+import 'babel-polyfill';
 import {
   createNoPokemons,
   createPokemonElements,
   setChild,
   resetInput
 } from './api/elements';
-import { getPokemonsByName, getAllPokemons } from './api/pokemons';
+import {
+  getPokemonsByName,
+  getAllPokemons,
+  sortPokemonsByName,
+  initPokemons
+} from './api/pokemons';
 
-// Query elements
-const searchInput = document.querySelector('.search__input');
-const resultsElement = document.querySelector('.results');
+initPokemons().then(start);
 
-// Get all pokemons
-const allPokemons = getAllPokemons();
+function start() {
+  // Query elements
+  const searchInput = document.querySelector('.search__input');
+  const resultsElement = document.querySelector('.results');
 
-// Reset input and results
-resetInput(searchInput);
-const allPokemonElements = createPokemonElements(allPokemons);
-setChild(resultsElement, allPokemonElements);
+  // Get all pokemons
+  getAllPokemons().then(allPokemons => {
+    // const allSortedPokemons = sortPokemonsByName(allPokemons);
+    const allSortedPokemons = sortPokemonsByName(allPokemons);
+    allPokemons();
+    resetInput(searchInput);
+    const allPokemonElements = createPokemonElements(allSortedPokemons);
+    setChild(resultsElement, allPokemonElements);
+  });
 
-// Add event listeners
-searchInput.addEventListener('input', event => {
-  const searchValue = event.target.value;
-  const pokemons = getPokemonsByName(searchValue);
-  const pokemonElements = createPokemonElements(pokemons);
+  // })
 
-  function createSearchCounter() {
-    const counter = pokemons.length;
-    return counter;
-  }
-  // setChild(resultsElement, pokemonElements);
-  console.log(pokemons);
-  if (pokemons.length > 0) {
-    setChild(resultsElement, pokemonElements);
-    console.log(createSearchCounter());
-    // const elementSearchCounter = document.createElement('div');
-    // elementSearchCounter.setAttribute.className('result__searchcount');
-    document.querySelector(
-      '.search__countinput'
-    ).innerHTML = createSearchCounter();
-    // createSearchCounter()
-    // parentElement(resultsElement, createSearchCounter());
-    const searchCounter = pokemonElements.length;
-  } else {
-    // const allPokemonElements = createPokemonElements(allPokemons);
-    setChild(results, createNoPokemons());
-  }
-});
+  // Reset input and results
+  resetInput(searchInput);
+  const allPokemonElements = createPokemonElements(allPokemons);
+  setChild(resultsElement, allPokemonElements);
 
-/**
- * Later, you can add sort functionality.
- */
+  // Add event listeners
+  searchInput.addEventListener('input', event => {
+    const searchValue = event.target.value;
+    const pokemons = getPokemonsByName(searchValue);
+    const pokemonElements = createPokemonElements(pokemons);
+
+    function createSearchCounter() {
+      const counter = pokemons.length;
+      return counter;
+    }
+    // setChild(resultsElement, pokemonElements);
+    console.log(pokemons);
+    if (pokemons.length > 0) {
+      setChild(resultsElement, pokemonElements);
+      console.log(createSearchCounter());
+      // const elementSearchCounter = document.createElement('div');
+      // elementSearchCounter.setAttribute.className('result__searchcount');
+      document.querySelector(
+        '.search__countinput'
+      ).innerHTML = createSearchCounter();
+      // createSearchCounter()
+      // parentElement(resultsElement, createSearchCounter());
+      const searchCounter = pokemonElements.length;
+    } else {
+      // const allPokemonElements = createPokemonElements(allPokemons);
+      setChild(results, createNoPokemons());
+    }
+  });
+
+  /**
+   * Later, you can add sort functionality.
+   */
+}
